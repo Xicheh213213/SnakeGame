4103f3351964da3b9e91ebecc7399859decbac7d
xicheh <sizyray@gmail.com>
2022-07-23 12:15:46 +0300

    Error2

---
+++ Source/SnakeGame/SnakeBase.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SnakeBase.h"
+#include "SnakeElementBase.h"
+// Sets default values
+ASnakeBase::ASnakeBase()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	ElementSize = 100.f;
+
+}
+
+// Called when the game starts or when spawned
+void ASnakeBase::BeginPlay()
+{
+	Super::BeginPlay();
+	AddSnakeElement(4);
+	
+}
+
+// Called every frame
+void ASnakeBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ASnakeBase::AddSnakeElement(int ElementsNum)
+{
+	for (int i = 0; i < ElementsNum; ++i)
+	{
+		FVector NewLocation(SnakeElements.Num() * ElementSize, 0, 0);
+		FTransform NewTransform(NewLocation);
+		auto NewSnakeElem = GetWorld()->SpawnActor<ASnakeElementBase>(SnakeElementClass, NewTransform);
+		SnakeElements.Add(NewSnakeElem);
+	}
+}
+
